7858de003184b0f21b6bb54d149631c1c3a7d97f
M4noel <manoelmurilo9@gmail.com>
2024-11-18 19:01:32 -0300

    teste-final

--- deploy-github-actions/wp-content/themes/new-tema/index.php
+++ deploy-github-actions/wp-content/themes/new-tema/index.php
@@ -3,7 +3,7 @@ get_header(); // Chama o cabeçalho do tema (header.php)
 ?>
 
 <main>
-    <h1>Bem-vindo ao meu tema WordPressx!</h1>
+    <h1>Bem-vindo ao meu tem deu bom ?!</h1>
     <p>Este é um tema básico para começar a desenvolver.</p>
 </main>
 

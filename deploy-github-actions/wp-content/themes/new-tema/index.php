426c39cdbd24970c711afa2d75954a1eeaefb445
M4noel <manoelmurilo9@gmail.com>
2024-11-18 19:11:11 -0300

    teste-milsssl

--- deploy-github-actions/wp-content/themes/new-tema/index.php
+++ deploy-github-actions/wp-content/themes/new-tema/index.php
@@ -4,7 +4,7 @@ get_header(); // Chama o cabeçalho do tema (header.php)
 
 <main>
     <h1>Bem-vindo ao meu tem deu bom d?!</h1>
-    <p>Este é um tema básico para começar a desenvosslveffr.</p>
+    <p>Este é um tema básico para começar a desenvosslveffssr.</p>
 </main>
 
 <?php

d7041dd1e00305d074235c1921c5d17edd493539
M4noel <manoelmurilo9@gmail.com>
2024-11-19 10:11:43 -0300

    sdsas

--- deploy-github-actions/wp-content/themes/new-tema/index.php
+++ deploy-github-actions/wp-content/themes/new-tema/index.php
@@ -4,8 +4,8 @@ get_header(); // Chama o cabeçalho do tema (header.php)
 
 <main>
     <h1>Bem-vindo ao meu tem deu bom d?!</h1>
-    <p>Este é um tema fffsssssbásssdddsfsssfssfiddssdssadscosss para começar sa desenvosslvsseffssr.</p>
-</main>
+    <p>Este é um temadsd fffsssssbásssdddsfsssfssfiddssdssadscosss para começar sa desenvosslvsseffssr.</p>
+</main>aa
 
 <?php
 get_footer(); // Chama o rodapé do tema (footer.php)

3674ebf0c6ca6280dff1701da03da81e4f288eea
M4noel <manoelmurilo9@gmail.com>
2024-11-19 10:10:04 -0300

    wewq

--- deploy-github-actions/wp-content/themes/new-tema/index.php
+++ deploy-github-actions/wp-content/themes/new-tema/index.php
@@ -4,7 +4,7 @@ get_header(); // Chama o cabeçalho do tema (header.php)
 
 <main>
     <h1>Bem-vindo ao meu tem deu bom d?!</h1>
-    <p>Este é um tema fffsssssbásssdddsfsssfssfiddssdsadscosss para começar sa desenvosslvsseffssr.</p>
+    <p>Este é um tema fffsssssbásssdddsfsssfssfiddssdssadscosss para começar sa desenvosslvsseffssr.</p>
 </main>
 
 <?php

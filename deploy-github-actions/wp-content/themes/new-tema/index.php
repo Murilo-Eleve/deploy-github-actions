784768c119de0e239cb4a365f3efd732c5768d92
M4noel <manoelmurilo9@gmail.com>
2024-11-19 09:26:25 -0300

    Alterações no index.php e arquivos necessários

--- deploy-github-actions/wp-content/themes/new-tema/index.php
+++ deploy-github-actions/wp-content/themes/new-tema/index.php
@@ -4,7 +4,7 @@ get_header(); // Chama o cabeçalho do tema (header.php)
 
 <main>
     <h1>Bem-vindo ao meu tem deu bom d?!</h1>
-    <p>Este é um tema bádddsffficosss para começar sa desenvosslvsseffssr.</p>
+    <p>Este é um tema fffbádddsffficosss para começar sa desenvosslvsseffssr.</p>
 </main>
 
 <?php

4d3e711f40ef624c90e91c2bfa219ad789e97dd1
M4noel <manoelmurilo9@gmail.com>
2024-11-19 12:03:26 -0300

    SASD

--- deploy-github-actions/wp-content/themes/new-tema/index.php
+++ deploy-github-actions/wp-content/themes/new-tema/index.php
@@ -5,7 +5,7 @@ get_header(); // Chama o cabeçalho do tema (header.php)
 <main>
     <h1>Bem-vindo ao meu tem deu bom d?!</h1>
     <p>Este é umddd temdsaffdfsdddaaaSDDaaasssaddsasdasddsdasdddddddddssssdadsdsd fffsssssbásssdddsfsssfssfiddssdssadscosss para começar sa desenvosslvsseffssr.</p>
-</main>aadd
+</main>aaddSADSD
 
 <?php
 get_footer(); // Chama o rodapé do tema (footer.php)
